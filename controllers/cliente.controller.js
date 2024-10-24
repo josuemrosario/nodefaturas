98e4d1b2bccf7150693c042ba23b4ea0934c7516
Josue Rosario <josue.rosario@escolar509.sp.senai.br>
2024-10-24 14:45:37 -0300

    18 - Criando CRUD para Clientes

--- controllers/cliente.controller.js
+++ controllers/cliente.controller.js
@@ -9,7 +9,8 @@ const validarCliente = [
 ];
 
 const mostrarClientes = async (req, res) => {
-  const query = { owner: req.session.userId };
+  console.log(req.session.userId)
+  const query = { usuarioRef: req.session.userId };
   const dadosclientes = await Cliente.find(query);
   res.render("pages/cliente", {
     titulo: "Clientes",
@@ -19,8 +20,69 @@ const mostrarClientes = async (req, res) => {
   });
 };
 
+const cadastrarCliente = async (req, res) => {
+  const errosValidacao = validationResult(req);
+  if (!errosValidacao.isEmpty()) {
+    const erros = errosValidacao.array();
+    req.flash("erros", erros);
+    req.flash("data", req.body);
+    return res.redirect("cadastrar");
+  }
+  const novoCliente = req.body;
+  novoCliente.usuarioRef = req.session.userId;
+  await Cliente.create(novoCliente);
+  req.flash("info", {
+    mensagem: "Cliente Criado",
+    type: "success",
+  });
+  res.redirect("/dashboard/clientes");
+};
+
+const editarCliente = async (req, res) => {
+  const clienteId = req.params.id;
+  const dadosCliente = await Cliente.findById(clienteId);
+  res.render('pages/cliente', {
+    titulo: 'Editar Cliente',
+    tipo: 'form',
+    formAction: 'editar',
+    dadosCliente: req.flash('data')[0] || dadosCliente,
+    erros: req.flash('errors'),
+  });
+};
+
+const atualizarCliente = async (req, res) => {
+  const errosValidacao = validationResult(req);
+  if (!errosValidacao.isEmpty()) {
+    const erros = errosValidacao.array();
+    req.flash('erros', erros);
+    req.flash('data', req.body);
+    return res.redirect('editar');
+  }
+  const clienteId = req.params.id;
+  const dadosCliente = req.body;
+  await Cliente.findByIdAndUpdate(clienteId, dadosCliente);
+  req.flash('info', {
+    mensagem: 'Cliente Atualizado',
+    tipo: 'successo'
+  });
+  res.redirect('/dashboard/clientes');
+};
+
+const deletarCliente = async (req, res) => {
+  const clienteId = req.params.id
+  await Cliente.findByIdAndDelete(clienteId);
+  req.flash('info', {
+    mensagem: 'Cliente excluido com sucesso',
+    tipo: 'successo'
+  });
+  res.redirect('/dashboard/clientes');
+};
 
 module.exports = {
-    mostrarClientes,
-    validarCliente,
+  mostrarClientes,
+  validarCliente,
+  cadastrarCliente,
+  editarCliente,
+  atualizarCliente,
+  deletarCliente
 };
--- routes/cliente.route.js
+++ routes/cliente.route.js
@@ -1,8 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
-    mostrarClientes,
-} = require('../controllers/cliente.controller');
+  mostrarClientes,
+  validarCliente,
+  cadastrarCliente,
+  editarCliente,
+  atualizarCliente,
+  deletarCliente
+} = require("../controllers/cliente.controller");
 
-router.get('/', mostrarClientes);
+router.get("/", mostrarClientes);
+
+router.get("/cadastrar", function (req, res) {
+  res.render("pages/cliente", {
+    titulo: "Cadastrar Cliente",
+    formAction: "cadastrar",
+    tipo: "form",
+    dadosCliente: req.flash("data")[0], 
+    erros: req.flash("erros"),
+  });
+});
+
+router.post("/cadastrar", validarCliente, cadastrarCliente);
+
+router.get('/:id/editar', editarCliente);
+router.post('/:id/editar', validarCliente, atualizarCliente);
+
+
+router.post('/:id/deletar', deletarCliente);
+
 module.exports = router;
